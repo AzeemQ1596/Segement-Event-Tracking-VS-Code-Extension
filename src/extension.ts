785b054dd4a8c658edd73ffc93861c39ea9863d7
AzeemQ1596 <azeem.quadri99@gmail.com>
2021-11-11 12:30:16 -0500

    snippet is working now

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 var fs = require('fs');
+import { AnyPtrRecord } from 'dns';
 import { TextEncoder } from 'util';
 import * as vscode from 'vscode';
 import { ExtensionContext, StatusBarAlignment, window, StatusBarItem, Selection, workspace, TextEditor, commands } from 'vscode';
@@ -24,14 +25,16 @@ export async function activate(context: vscode.ExtensionContext) {
 		const fs = require('fs');
 		let oS = os.platform();
 		console.log(oS);
-		
+		/*
 		const homedir = require('os').homedir();
 		let snippetPath = homedir.toString().concat("\\code\\segment-event-tracking\\segment_snippets.json");
 		let snippetUri = vscode.Uri.file(snippetPath);
 		const encoder = new TextEncoder();
 		//workspace.fs.writeFile(snippetUri, encoder.encode(" "));
-		require('fs').writeFileSync(snippetPath, "sgdihs;fsd");	
+		require('fs').writeFileSync(snippetPath, "azeem");	*/
 		
+
+
 		await getFiles().then(async filePaths => {
 		if(filePaths === null) {
 
@@ -41,14 +44,49 @@ export async function activate(context: vscode.ExtensionContext) {
 			else {
 					vscode.window.showInformationMessage(`${filePaths}`);
 					let documentText = await readFiles(filePaths);
-					for(var index = 0; index < documentText.length; index++){
+					
+					let wsPath: string;
+					let ws = workspace.workspaceFolders;
+					if(ws!== undefined) { 
+
+						wsPath = ws[0].uri.fsPath;
 						
-						searchText(documentText[index]);
-					};
+
+						vscode.window.showInformationMessage(`${wsPath}`);
+					
+						require('fs').writeFileSync(wsPath.concat("\\segmentEventTable.json"), "{ \n", function (err: any) {
+							if (err) {throw err;}
+							console.log('File is created successfully.'); });
+						
+						require('fs').writeFileSync(wsPath.concat("\\.vscode\\segmentSnippet.code-snippets"), "{ \n", function (err: any) {
+							if (err) {throw err;}
+							console.log('File is created successfully.'); });
+
+						for(var index = 0; index < documentText.length; index++){
+						
+							let e = searchText(documentText[index], filePaths[index]);
+							
+
+							require('fs').appendFile(wsPath.concat("\\.vscode\\segmentSnippet.code-snippets"), `${e}`, function (err: any) {
+								if (err) {throw err;}
+								console.log('File is created successfully.'); });
+							
+							if(index!==documentText.length) {
+								require('fs').appendFile(wsPath.concat("\\.vscode\\segmentSnippet.code-snippets"), ",", function (err: any) {
+								if (err) {throw err;}
+								console.log('File is created successfully.'); });
+							}	
+						};
+						
+						require('fs').appendFile(wsPath.concat("\\.vscode\\segmentSnippet.code-snippets"), "}", function (err: any) {
+							if (err) {throw err;}
+							console.log('File is created successfully.'); });
+						//vscode.window.showInformationMessage(`${event_array}`);
+					}
+			
 				};
 				
-			});
-			
+			});	
 	});
 	context.subscriptions.push(disposable);
 }
@@ -83,66 +121,19 @@ async function readFiles(paths: string[]): Promise<string[]> {
 
 }
 
-async function searchText(docText: string) {
+function searchText(docText: string, filePaths: string): string[] {
 
 	//let obj = JSON.parse(jsonString);
-	// 1) Identify
-	interface identify_obj {
-		userId : string;
-		type : string;
-		file_name : number;
-		line_beginning : number;
-		line_end : string;
-	}
-
-	// 2) Group
-	interface group_obj {
-		groupId : string;
-		type : string;
-		file_name : number;
-		line_beginning : number;
-		line_end : string;
-	}
-
-	// 1&2) Both can have traits that we can add 
-	interface trait_obj {
-		trait : string
-	}
-
-	// 3) Track
-	interface track_obj {
-		event_name : string;
-		type : string;
-		file_name : number;
-		line_beginning : number;
-		line_end : string;
-		property : string[];
-	}
-
-	// 4) Page
-	interface page_obj {
-		event_name : string;
-		category : string;
-		type : string;
-		file_name : number;
-		line_beginning : number;
-		line_end : string;
-		property : string[];
-	}
-
 	
 	// regex to see if it's a valid segment event
 	let segment_indicator = /(analytics.)((.|\n|))*?(;\n)/g;
 	
-	// let code be a string 
-	let code: string[];
+	let eventArray: string[] = [];  // declaring an array to hold event details
 	
 	// see if the code we brought in; docText fits the regex
 	let searchResult = docText.match(segment_indicator);
-	console.log(`the search result code blocK: " + ${searchResult}`);
+	//console.log(`the search result code blocK: " + ${searchResult}`);
 	
-	let events: string[]; // declaring an array to hold event details
-
 	// 1. find what kind of segment event it is and if it matches the format of a segment event
 	// 2. if yes, then cut the code down to the properties only
 	// 3. using match() function, find a list of all the properties or if it is empty
@@ -154,43 +145,73 @@ async function searchText(docText: string) {
 	let event_name: string;
 	// if the code has the template of a generic segment event, we now check if they are one of the four types of segment events:
 	searchResult?.forEach(item => {
+		
+		let event_indicator = /(?<=\(\").+?(?=\"\,)/g;
+		let prop_indicator = /(?<={)((.|\n|))*?(?=})/g;
+
+		let name = item.match(event_indicator); // manipulating for segment event name and storing it in event_name
+		//console.log(`The event name we extracted by using regex: ${name}`);
 
 		if (is_identify.exec(item)) {
-			return null;
-
-		} if (is_track.exec(item)) {
-			// Output: an array and return with: event name, properties, event type
-			// so we can now find the file name and line number
-		
-			// 1) Find event name or if it's null
-			let track_event_indicator = /(?<=\(\").+?(?=\"\,)/; // regex to manipulate for segment event name
 			
-			let name = track_event_indicator.exec(item); // manipulating for segment event name and storing it in event_name
-			console.log(`The event name we extracted by using regex: ${name}`);
-			//track_event.push(event_name); // adding the event name to the array
-			//console.log("The track event array after adding the event name: " + track_event);
-			return name;
-
-		}
-
-			//let obj: MyObj = JSON.parse('{ "eventName": e}') taking this out for now
-			// 2) Find properties
-			/*
-			for (let i = 0; i < code.length; i++) {
+			let snip = 
+			   `${escape(`${item}`)}: {
+				"prefix": ["analytics"],
+				"body": ${escape(`${item}`)},
+				"event_name": ${escape(`${name?.toString()}`)},
+				"type" : "identify",
+				"file_path" : ${escape(`${filePaths}`)},
+				"line_number" : " "
+			   }`;			
 				
-				i++
-			}*/
+			eventArray.push(snip);
+	
+		} if (is_track.exec(item)) {
 			
-		if (is_page.exec(docText)) {
-			return null;
-
-		} if (is_group.exec(docText)) {
-			return null;
+			let prop = item.match(prop_indicator);
+			let newItem = (item.replace(/"/g, "'")).replace(/(\r\n|\n|\r)/gm, "");
+			let snip = 
+			`"${name?.toString()}": {
+				"prefix": ["analytics"],
+				"body": "${newItem}",
+			   }`;
+			
+			eventArray.push(snip);
 
 		}
-	
-	});
-	
+
+		if (is_page.exec(item)) {
+			let prop = item.match(prop_indicator);
+			let snip = 
+			  `${escape(`${item}`)}: {
+				"prefix": ["analytics"],
+				"body": ${escape(`${item}`)},
+				"event_name": ${escape(`${name?.toString()}`)},
+				"type" : "identify",
+				"property" : ${escape(`${prop?.toString()}`)},
+				"file_path" : ${escape(`${filePaths}`)},
+				"line_number" : " "
+			   }`;
+			
+			eventArray.push(snip);
+
+		} if (is_group.exec(item)) {
+			
+			let snip = 
+			`${escape(`${item}`)}: {
+				"prefix": ["analytics"],
+				"body": ${escape(`${item}`)},
+				"event_name": ${escape(`${name?.toString()}`)},
+				"type" : "identify",
+				"file_path" : ${escape(`${filePaths}`)},
+				"line_number" : " "
+			   }`;
+
+			eventArray.push(snip);
+		}
+
+	});	
+	return eventArray;
 	}
 
 // this method is called when your extension is deactivated
