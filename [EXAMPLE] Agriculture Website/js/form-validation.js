6f2238bffafa2a6eda5f590da5c6be34a4214060
AzeemQ1596 <azeem.quadri99@gmail.com>
2021-11-18 18:02:58 -0500

    fixed errors of example files

--- [EXAMPLE] Agriculture Website/js/form-validation.js
+++ [EXAMPLE] Agriculture Website/js/form-validation.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 isAction.analytics.track("7sales_account_connected", { 
     plan: "spotify", accountType: "logistics"});
   
@@ -5,7 +6,7 @@ isAction.analytics.track("7sales_account_connected", {
       $("#search-box").submit(function(event) {
           
           var search_term = $("#search-term").val(); //search_term variable takes in the value from "search-term"
-          if($.trim(search_term) == '') { //checks if it's empty or whitespace
+          if($.trim(search_term) === '') { //checks if it's empty or whitespace
               event.preventDefault();
               alert("Please enter valid input");
               return false;
@@ -17,7 +18,7 @@ isAction.analytics.track("7sales_account_connected", {
         plan: "spotify", accountType: "logistics"});
         isAction.analytics.group("finance");
   
-  })
+  });
   
   
   isAction.analytics.page("page2");
