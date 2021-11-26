58132598f03cbc2b8ca98ffadf479e651d6c4365
eunicechoi98 <eunice.choi@queensu.ca>
2021-11-26 15:28:51 -0500

    hello

--- src/makeWebview.ts
+++ src/makeWebview.ts
@@ -3,11 +3,13 @@ import * as vscode from 'vscode';
 import * as path from 'path';
 import { ExtensionContext, StatusBarAlignment, window, StatusBarItem, Selection, workspace, TextEditor, commands } from 'vscode';
 
-export function getWebviewContent(json_file: vscode.Uri) {
+export function getWebviewContent(json_file_path) {
     // html
   // <th> = columns
   // <td> = contents
-  //let segment_event_rows = loadContent(json_file);
+
+  var segment_event_rows_array = require(json_file_path);
+  let segment_event_rows = loadContent(segment_event_rows_array);
   // 
 
   let my_html = `<!DOCTYPE html>
@@ -15,42 +17,59 @@ export function getWebviewContent(json_file: vscode.Uri) {
   <head>
 	  <meta charset="UTF-8">
 	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="jquery-3.4.1.min.js"></script>
 	  <title>Your Segment Event Library</title>
   </head>
   <body>
     <table>
       <tr>
         <th>Event ID</th>
-        <th>Type</th>
+        <th>Category</th>
         <th>Event Name</th>
-        <th>Property</th>
         <th>Code</th>
+        <th>Type</th>
+        <th>Property</th>
         <th>File Path</th>
       </tr>
       <tr>
-        <td>Alfreds Futterkiste</td>
-        <td>Maria Anders</td>
-        <td>Germany</td>
-        <td>Alfreds Futterkiste</td>
-        <td>Maria Anders</td>
-        <td>Germany</td>
-      </tr></table></body></html>`;
+        <td>tr_10s_680</td>
+        <td>null</td>
+        <td>10sales_account_connected</td>
+        <td>analytics.track('10sales_account_connected',{plan:'spotify',accountType:'logistics'});</td>
+        <td>track</td>
+        <td>property":"plan:'spotify',accountType:'logistics'</td>
+        <td>jquery-3.4.1.min.js</td>
+      </tr>` + segment_event_rows +`</table></body></html>`
 	return my_html;
   }
 
-// Builds the HTML Table out of myList.
 /*
-export function loadContent(json_file: string) {
+{"eventID":"tr_10s_680",
+  "category":"",
+  "eventName":"10sales_account_connected'",
+  "code":"analytics.track('10sales_account_connected',{plan:'spotify',accountType:'logistics'});",
+  "type":"track",
+  "property":"plan:'spotify',accountType:'logistics'",
+  "filePath":"jquery-3.4.1.min.js"},
+
+
+*/
+
+
+// Builds the HTML Table out of myList.
+export function loadContent(json_file) {
   let html = ``;
   for (var i = 0; i < json_file; i++) {
     var row_start$= $(`<tr>`);
+    let content = ``;
     for (var j = 0; j <json_file[i]; j++) {
-        if 
+      content + ``;
+        
     }
     var row_end$ = $(`</tr>`);
+    let row = row_start$ + content + row_end$;
+    html + row_end$;
   }
 
 }
 
-*/
+
